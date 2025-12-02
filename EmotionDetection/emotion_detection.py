7f3e57ea4f76005e0dfba00074bab6c5b27933ff
Sadiq Adedayo <adedayoas91@gmail.com>
2025-12-02 14:47:05 -0500

    package emotion detector

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,12 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input = { "raw_document": { "text": text_to_analyse } }    
+    response = requests.post(URL, json = Input, headers=Headers)    
+    formatted_response = json.loads(response.text)
+    # Extracting sentiment label and score from the response
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    
+    return emotion
